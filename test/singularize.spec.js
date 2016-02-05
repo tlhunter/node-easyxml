db95edf2c5feadbabb2bf8ea7dfd6b3eabc78957
Thomas Hunter II <me@thomashunter.name>
2016-02-05 00:57:22 -0800

    Rebuilding test suite as unique tests
    Removes the JSON fixtures which were too limiting
    Allows for adding .only to individual tests
    Fixes #14

---
+++ test/singularize.spec.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var assert  = require('assert');
+var fs = require('fs');
+var EasyXml = require('../index.js');
+
+describe("Singularize Children", function () {
+  it("should parse a JSON object without singularizeChildren to XML", function() {
+    var easyXML = new EasyXml({
+      singularizeChildren: false
+    });
+
+    var before = {
+      CmsUsers: [
+        { user: "user1" },
+        { user: "user2" }
+      ]
+    };
+
+    var after = easyXML.render(before);
+
+    var expected = fs.readFileSync('./test/fixtures/singularizeChildren.xml', 'utf8');
+
+    assert.strictEqual(after, expected);
+  });
+
+  it("should parse a JSON object without singularizeChildren to XML (with object)", function() {
+    var easyXML = new EasyXml({
+      singularizeChildren: false
+    });
+
+    var before = {
+      CmsUsers: [
+        { user: { name: 'user1' } },
+        { user: { name: 'user2' } }
+      ]
+    };
+
+    var after = easyXML.render(before);
+
+    var expected = fs.readFileSync('./test/fixtures/singularizeChildren2.xml', 'utf8');
+
+    assert.strictEqual(after, expected);
+  });
+
+  it("should parse a JSON object with correct captalization", function() {
+    var easyXML = new EasyXml({
+      singularizeChildren: true
+    });
+
+    var before = {
+      Users: [
+        { name: "user1" },
+        { name: "user2" }
+      ]
+    };
+
+    var after = easyXML.render(before);
+
+    var expected = fs.readFileSync('./test/fixtures/singularizeChildren3.xml', 'utf8');
+
+    assert.strictEqual(after, expected);
+  });
+
+  it("should parse a JSON object and correctly nest child nodes", function() {
+    var easyXML = new EasyXml({
+      singularizeChildren: false
+    });
+
+    var before = {
+      CmsUsers: [
+        {
+          user: {
+            name: "user1",
+            email: "user1@example.com"
+          }
+        },
+        {
+          user: {
+            name: "user2",
+            email: "user2@example.com"
+          }
+        }
+      ]
+    };
+
+    var after = easyXML.render(before);
+
+    var expected = fs.readFileSync('./test/fixtures/singularizeChildrenMultipleProperties.xml', 'utf8');
+
+    assert.strictEqual(after, expected);
+  });
+});
